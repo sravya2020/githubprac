ed870fe9cc8441d148e524d2370c8409055e9f97
sravya <sravyabrn2020@gmail.com>
2020-01-21 13:01:33 +0530

    add name

--- githubprac/ViewController.swift
+++ githubprac/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    var surname = "srav"
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
