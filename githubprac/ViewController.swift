551a0025b08f6e2d22180c526871add9b970fcec
sravya <sravyabrn@gmail.com>
2020-01-21 12:53:12 +0530

    Initial Commit

---
+++ githubprac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  githubprac
+//
+//  Created by M Sravya on 1/21/20.
+//  Copyright © 2020 M Sravya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
